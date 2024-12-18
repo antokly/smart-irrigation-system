ea4fcfe86777b9afbc48b4b5ac4c08057ff88d72
antoninklyk <antonin.kulyk@outlook.fr>
2024-12-18 19:40:22 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,46 @@
-/* Le but de ce programme est de nous donner 
-la mesure de l'humiditié par le sensor toutes les 1 s */
-// Just with WiFi classes sufficient
-
 #include <Arduino.h>
 #include "Sensors.h"
 #include "Actuators.h"
 #include "Network.h"
-#include "SendHTTPWebServer.h"
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Configuration MQTT
+const char* mqtt_server = "192.168.1.178"; // Adresse IP de votre broker MQTT
+const int mqtt_port = 1883;               // Port MQTT (par défaut : 1883)
+const char* mqtt_topic = "/test/topic";   // Sujet utilisé pour les messages
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Callback pour recevoir les messages
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message reçu sur le sujet : ");
+  Serial.println(topic);
+
+  Serial.print("Contenu : ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+// Connexion au broker MQTT
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connexion au broker MQTT...");
+    if (client.connect("ESP32Client")) { // Nom du client
+      Serial.println("Connecté !");
+      client.subscribe(mqtt_topic);      // Souscription au sujet
+    } else {
+      Serial.print("Echec, rc=");
+      Serial.print(client.state());
+      Serial.println(" Nouvelle tentative dans 5 secondes...");
+      delay(5000);
+    }
+  }
+}
 
 void setup(void)
 {
@@ -18,15 +52,32 @@ void setup(void)
 
   //network init
   network_setup();
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop(void)
 {
   //Measure Humidity with Sensors
   sensor_data_acquisition();
-  
-  //WebPage Send Data
-  http_send_data_to_web_page(String(humidity));
+
+  //Com MQTT
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // MQTT msg post
+  static unsigned long lastMsg = 0;
+  unsigned long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    String message = "Hello, MQTT!";
+    Serial.print("Envoi du message : ");
+    Serial.println(message);
+    client.publish(mqtt_topic, message.c_str());
+  }
 
   //TO DO Grafana with influxdb to visualize datas on a dashboard
 
